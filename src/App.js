cb2f9327e8c41a6c9aaa9dc3c0e6d9e781423f8d
LiamNorman <liam.norman@zando.co.za>
2017-06-10 18:34:15 +0200

    added basic todo functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Todotick
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            todos: [
+                {
+                    id: 1,
+                    name: 'Learn JSX',
+                    isComplete: true
+                },
+                {
+                    id: 2,
+                    name: 'Learn React',
+                    isComplete: false
+                },
+                {
+                    id: 3,
+                    name: 'Develop Todotick',
+                    isComplete: false
+                },
+                {
+                    id: 4,
+                    name: 'Run Travis',
+                    isComplete: false
+                },
+                {
+                    id: 5,
+                    name: 'Ship it!',
+                    isComplete: false
+                }
+            ],
+            currentTodo: ''
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(evt) {
+        this.setState({
+            currentTodo: evt.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+                <h2> React Todos </h2>
+                <div className="Todo-App">
+                    <form>
+                        <input type="text" onChange={this.handleInputChange} value={this.state.currentTodo}/>
+                    </form>
+                    <div className="Todo-List">
+                        <ul>
+                            {this.state.todos.map(todo =>
+                                <li key={todo.id}>
+                                  <input type="checkbox" defaultChecked={todo.isComplete}/>{todo.name}</li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
